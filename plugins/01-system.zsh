ea1a25f8b53d4f5f69fa119d7e23483dc5a1a377
Ash <contact@ash.fail>
2024-07-16 22:12:08 +0200

    fix: zsh plugins and their paths are stupid

--- plugins/01-system.zsh
+++ plugins/01-system.zsh
@@ -1,9 +1,13 @@
+# shellcheck disable=SC1090
 plugins=('zsh-autosuggestions' 'zsh-completions' 'zsh-syntax-highlighting')
 
 for plugin in "${plugins[@]}"; do
     for subdir in '/' 'zsh/' 'zsh/plugins/'; do
-        # shellcheck disable=SC1090
-        [[ -f "/usr/share/${subdir}${plugin}/${plugin}.plugin.zsh" ]] \
-            && source "/usr/share/${subdir}${plugin}/${plugin}.plugin.zsh"
+        plugin="/usr/share/${subdir}${plugin}/${plugin}"
+        [[ -f "${plugin}.plugin.zsh" ]] \
+            && source "${plugin}.plugin.zsh" && break
+
+        [[ -f "${plugin}.zsh" ]] \
+            && source "${plugin}.zsh" && break
     done
 done
