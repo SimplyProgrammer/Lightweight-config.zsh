021e7eb6e597c22b1dd7e8b6e31734f2e5544268
Ash <contact@ash.fail>
2024-09-09 13:29:13 +0200

    fix: loading system plugins

--- plugins/01-system.zsh
+++ plugins/01-system.zsh
@@ -3,11 +3,11 @@ plugins=('zsh-autosuggestions' 'zsh-completions' 'zsh-syntax-highlighting')
 
 for plugin in "${plugins[@]}"; do
     for subdir in '/' 'zsh/' 'zsh/plugins/'; do
-        plugin="/usr/share/${subdir}${plugin}/${plugin}"
-        [[ -f "${plugin}.plugin.zsh" ]] \
-            && source "${plugin}.plugin.zsh" && break
+        plugin_path="/usr/share/${subdir}${plugin}/${plugin}"
+        [[ -f "${plugin_path}.plugin.zsh" ]] \
+            && source "${plugin_path}.plugin.zsh" && break
 
-        [[ -f "${plugin}.zsh" ]] \
-            && source "${plugin}.zsh" && break
+        [[ -f "${plugin_path}.zsh" ]] \
+            && source "${plugin_path}.zsh" && break
     done
 done
