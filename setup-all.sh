69b5799bfe91abeeea4a8b54d1eff281025c6fa9
Programmer001 <39063786+SimplyProgrammer@users.noreply.github.com>
2026-08-05 01:41:45 +0200

    Refactor user creation logic in setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -61,21 +61,22 @@ create_admin_user() {
 
     if id "$USR" >/dev/null 2>&1; then
         echo "User '$USR' already exists. Skipping creation."
-    else
-        if grep -q '^sudo:' /etc/group; then
-           ADMIN_GROUP=sudo
-        elif grep -q '^wheel:' /etc/group; then
-            ADMIN_GROUP=wheel
-        fi
+        exit 0
+    fi
     
-        if command -v adduser >/dev/null 2>&1; then
-            $SUDO adduser --ingroup "$ADMIN_GROUP" --shell "$ZSH_PATH" "$USR"
-        elif command -v useradd >/dev/null 2>&1; then
-            $SUDO useradd -m -g "$ADMIN_GROUP" -s "$ZSH_PATH" "$USR"
-        else
-            echo "No user creation utility found."
-            exit 1
-        fi
+    if grep -q '^sudo:' /etc/group; then
+        ADMIN_GROUP=sudo
+    elif grep -q '^wheel:' /etc/group; then
+        ADMIN_GROUP=wheel
+    fi
+
+    if command -v adduser >/dev/null 2>&1; then
+        $SUDO adduser --ingroup "$ADMIN_GROUP" --shell "$ZSH_PATH" "$USR"
+    elif command -v useradd >/dev/null 2>&1; then
+        $SUDO useradd -m -g "$ADMIN_GROUP" -s "$ZSH_PATH" "$USR"
+    else
+        echo "No user creation utility found."
+        exit 1
     fi
 
     echo "User '$USR' created."
